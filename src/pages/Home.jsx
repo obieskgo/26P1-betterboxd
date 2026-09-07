04d61b43aed5bff2ca6c114c49a46a6af3ba04fe
Go Obie <obieseango08@gmail.com>
2026-09-07 11:25:12 +0800

    Added own titles/ pictures to NoAPI version

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,19 @@ import Moviecard from "../components/Moviecard"
 import {useState} from "react"
 import "../css/Home.css"
 
+import SGDLR1 from "../assets/SGDLR1.webp"
+import SGDLR2 from "../assets/SGDLR2.webp"
+import SGDLRBA from "../assets/SGDLRBA.webp"
+import SGDLR3 from "../assets/SGDLR3.webp"
+
 function Home(){
   const[searchQuery, setSearchQuery] = useState(""); // state, fcn
 
   const movies =[
-    {id: 1, title:"代理人", release_date: "2021"},
-    {id: 2, title:"时光代理人 第二季", release_date: "2023"},
-    {id: 3, title:"时光代理人 英都篇", release_date: "2024"},
-    {id: 4, title:"时光代理人 第三季", release_date: "2026"},
+    {id: 1, title:"时光代理人 Link Click", release_date: "2021",url:SGDLR1},
+    {id: 2, title:"时光代理人 Link Click S2", release_date: "2023",url:SGDLR2},
+    {id: 3, title:"时光代理人 Link Click: Bridon Arc", release_date: "2024",url:SGDLRBA},
+    {id: 4, title:"时光代理人 Link Click S3", release_date: "2026",url:SGDLR3},
   ];
 
   const handleSearch = (e) => {
